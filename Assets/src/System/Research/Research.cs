ee1d21b082996ece2066dd10f924f201cf91d3ad
djih <djimon@gmx.de>
2024-10-02 22:51:27 +0200

    Vorbereitung Researching

---
+++ Assets/src/System/Research/Research.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Research
+{
+    public string Name;
+    public Sprite Image;
+    public int GoldCost;
+    public Dictionary<string, int> DNACostDict;
+    public int DurationInRounds;
+    public int remainingRounds;
+    public bool researchFound = false;
+    public bool researchStarted = false;
+    public bool researchCompleted = false;
+
+    public Research(string name, Sprite image, int goldCost, Dictionary<string, int> dnaCostDict, int rounds)
+    {
+        Name = name;
+        Image = image;
+        GoldCost = goldCost;
+        DNACostDict = dnaCostDict;
+        DurationInRounds = rounds;
+    }
+
+    public void DoResearch()
+    {
+        if (researchFound && !researchStarted)
+        {
+            researchStarted = true;
+            remainingRounds = DurationInRounds;
+        }
+        else if(researchFound && researchStarted)
+        {
+            remainingRounds--;
+        }
+    }
+
+    
+}
+
---
+++ Assets/src/System/Research/Researchmanager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Researchmanager : MonoBehaviour
+{
+    public List<Research> activeResearches;
+    public List<Research> allResearches;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
